f3b527bb27a81658114167732711700eb4a9a6d9
Odimac84 <bjorn.nilsson1984@gmail.com>
2021-11-24 20:06:02 +0000

    added context and possibility to add to cart

--- cart/views.py
+++ cart/views.py
@@ -3,11 +3,12 @@ from django.shortcuts import render, redirect
 def view_cart(request):
     return render(request, 'cart/cart.html')
 
+
 def add_to_cart(request, item_id):
     
     quantity = int(request.POST.get('quantity'))
     redirect_url = request.POST.get('redirect_url')
-    cart = request.session.get('bag', {})
+    cart = request.session.get('cart', {})
 
     if item_id in list(cart.keys()):
         cart[item_id] += quantity
