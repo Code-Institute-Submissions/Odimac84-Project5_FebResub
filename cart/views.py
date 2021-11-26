ac6d9116fb4d4700850c8b31d4cb5ecbed8ea88d
Odimac84 <bjorn.nilsson1984@gmail.com>
2021-11-26 20:06:08 +0000

    added messages to toasts for pruduct handling in customer view

--- cart/views.py
+++ cart/views.py
@@ -8,16 +8,17 @@ def view_cart(request):
 
 def add_to_cart(request, item_id):
     
-    product = Product.objects.get(pk=item_id)
+    product = get_object_or_404(Product, pk=item_id)
     quantity = int(request.POST.get('quantity'))
     redirect_url = request.POST.get('redirect_url')
     cart = request.session.get('cart', {})
 
     if item_id in list(cart.keys()):
         cart[item_id] += quantity
+        messages.success(request, f'Updated quantity  {product.name} to {cart[item_id]} in your bag')
     else:
         cart[item_id] = quantity
-        messages.success(request, f'{product.name} added to your cart')
+        messages.success(request, f'{product.name} has been added to your cart')
 
     request.session['cart'] = cart
     return redirect(redirect_url)
@@ -25,13 +26,16 @@ def add_to_cart(request, item_id):
 
 def change_cart(request, item_id):
     
+    product = get_object_or_404(Product, pk=item_id)
     quantity = int(request.POST.get('quantity'))
     cart = request.session.get('cart', {})
 
     if quantity > 0:
         cart[item_id] = quantity
+        messages.success(request, f'Updated quantity  {product.name} to {cart[item_id]} in your bag')
     else:
         cart.pop(item_id)
+        messages.success(request, f'{product.name} has been removed from your cart')
     
     request.session['cart'] = cart
     return redirect(reverse('view_cart'))
@@ -44,7 +48,7 @@ def remove_from_cart(request, item_id):
         product = get_object_or_404(Product, pk=item_id)
         cart = request.session.get('cart', {})
         cart.pop(item_id)
-        messages.success(request, f'{product.name} removed from cart')
+        messages.success(request, f'{product.name} has been removed from your cart')
 
         request.session['cart'] = cart
         return HttpResponse(status=200)
