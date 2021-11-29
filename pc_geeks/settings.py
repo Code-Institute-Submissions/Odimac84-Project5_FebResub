45ee3aa591d127b4fdf890fd4bd172471eb88fbf
Odimac84 <bjorn.nilsson1984@gmail.com>
2021-11-29 21:31:23 +0000

    added crispy-forms and started to build the checkout.html

--- pc_geeks/settings.py
+++ pc_geeks/settings.py
@@ -45,6 +45,8 @@ INSTALLED_APPS = [
     'products',
     'cart',
     'checkout',
+    'crispy_forms',
+
     
 ]
 
@@ -59,6 +61,8 @@ MIDDLEWARE = [
 ]
 
 ROOT_URLCONF = 'pc_geeks.urls'
+
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 TEMPLATES = [
     {
@@ -76,6 +80,10 @@ TEMPLATES = [
                 'django.contrib.messages.context_processors.messages',
                 'cart.context.cart_contents',
             ],
+            'builtins': [
+                'crispy_forms.templatetags.crispy_forms_tags',
+                'crispy_forms.templatetags.crispy_forms_field',
+            ]
         },
     },
 ]
